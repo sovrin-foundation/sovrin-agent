b7e02d5e83434c6bfb04e2a91564d72294ebba11
Khagesh Sharma <khageshhiet@gmail.com>
2016-11-11 19:24:25 +0530

    x

--- pre-push.sh
+++ pre-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ../../ && pytest &> /dev/null
+cd ../../ && pytest
 if [ $? -ne 0 ]; then
     echo "This commit is rejected due to failing tests. Please fix them and try again."
     exit 1
