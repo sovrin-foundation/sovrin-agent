fbefc5a8bbdf7d43c0cb49edc4f71d5ab4c72b22
Khagesh Sharma <khageshhiet@gmail.com>
2016-11-11 17:52:17 +0530

    Updated pre-push hook

--- pre-push.sh
+++ pre-push.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
-pytest || exit 1
+#!/bin/bash
+cd ../../ && pytest &> /dev/null
+if [ $? -ne 0 ]; then
+    echo "This commit is rejected due to failing tests. Please fix them and try again."
+    exit 1
+fi
 
+exit 0
