52eabe2f08257b5022a0c39bacd57fe44654ac2c
Jason Law <jason@evernym.us>
2017-01-30 20:17:26 -0700

    adjusted requirements versions

--- agent/extensions/LocationV0.py
+++ agent/extensions/LocationV0.py
@@ -3,11 +3,11 @@ from aiohttp.web_reqrep import json_response
 
 extension_designator = "location-v0.notsodistantfuture.com"
 
+
 def get_app(loop):
     async def timezone_handler(request):
         timezone_bundle = {}
         return json_response(data=timezone_bundle)
-
 
     locationapp = Application(loop=loop)
     locationapp.router.add_get('/timezone', timezone_handler)
